b68f9ee59ff99012aaf8aa1f235bddd3389afc10
iamjing66 <iamjing66@163.com>
2021-12-10 14:07:50 +0800

    update

--- watch.py
+++ watch.py
@@ -5,34 +5,28 @@ import requests
 
 
 def get_message():
-    url = "https://www.ems.com.cn/apple/getMailNoRoutes"
-
-    data = {
-        "mailNum": "EZ341746550CN"
-        }
-
+    url = "https://m.kuaidi100.com/query"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
-        "contentType": "application/x-www-form-urlencoded;charset=utf-8"
-        }
-
-    # res = requests.get(url=url, headers=headers, json=data)
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+        "Cookie": "csrftoken=dWbSdIkipfja3EilDuGRecAYQtsJP3QVLbQhyUvN2NA; WWWID=WWWB8870091F52FA59F771F801FE5040595",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+    }
+    data = {
+        "postid": "JDX006601212979",
+        "id": "1",
+        "valicode": "",
+        "temp": "0.4556408500954994",
+        "type": "jd",
+        "phone": "",
+        "token": "",
+        "platform": "MWWW"
+    }
     res = requests.post(url, headers=headers, data=data).json()
-    result = res["trails"][0]
-    url_data = {
-        "mail_no": "",
-        "optime": "",
-        "opcode": "",
-        "opreate_type": "",
-        "process": "",
-        }
-    i = result[0]
-    url_data["optime"] = i["optime"]
-    url_data["opcode"] = i["opCode"]
-    url_data["opreate_type"] = i["opreateType"]
-    url_data["process"] = i["processingInstructions"]
-    msg = str(url_data['optime'])+'\n'+url_data['opreate_type']+'\n'+url_data['process']
-    bark_Url = f"https://api.day.app/cSUVV3HyLE5Fkw4Jx33EW8/{'iphone13 pro 快递'}/{msg}"
+    order = res["nu"]
+    last_time = res["data"][0]["time"]
+    last_info = res["data"][0]["context"]
+    msg = f"订单号：{}\n最新时间：{}\n最新信息：{}".format(order, last_info, last_time)
+    bark_Url = f"https://api.day.app/cSUVV3HyLE5Fkw4Jx33EW8/{'AirPods pro'}/{msg}"
     requests.get(bark_Url)
     print((datetime.datetime.now()).strftime("%dd %H:%M:%S"))
 
